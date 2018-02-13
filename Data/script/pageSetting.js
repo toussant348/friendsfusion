2e372f9d234e23bda04aa2d9420208a595186b00
toussant <toussant3482131@gmail.com>
2018-02-12 22:29:21 -0500

    continue

--- Data/script/pageSetting.js
+++ Data/script/pageSetting.js
@@ -4,6 +4,9 @@ let side_bar_height = window.innerHeight;
 const header_height = 20;
 const footer_height = 10;
 
-//side_bar_height-=(header_height + footer_height);
 
+setSideBarHeight(){
+    side_bar_height-=(header_height + footer_height);
+    document.getElementsByClassName("sideBar")[0].setAttribute
+}
 console.log(side_bar_height);
